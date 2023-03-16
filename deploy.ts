708305761366d771f60d2d575f18299cd2aaaeed
Toasticle <92263505+Toasticle@users.noreply.github.com>
2023-03-16 22:25:04 +0000

    Add files via upload

--- deploy.ts
+++ deploy.ts
@@ -3,35 +3,29 @@ addEventListener('fetch', (event) => {
 });
 
 async function handleRequest(request: Request): Promise<Response> {
-  // Extract the path from the incoming request URL
   const url = new URL(request.url);
   let path = url.pathname;
 
-  // If the path is a directory, append index.html
-  if (path.endsWith('/')) {
-    path += 'index.html';
+  if (path === '/') {
+    path = '/index.html';
   }
 
-  // Get the file extension from the path
-  const extension = path.split('.').pop();
-  
-  // Set the content type based on the file extension
-  const contentType = getContentType(extension);
+  const response = await fetch(`https://raw.githubusercontent.com/Toasticle/test123/main${path}`);
 
-  // Construct the URL for the remote file
-  const remoteUrl = `https://raw.githubusercontent.com/Toasticle/test123/main${path}`;
-
-  // Fetch the remote file and return it as the response
-  const response = await fetch(remoteUrl);
-
-  // Set the content type header
   const headers = new Headers(response.headers);
-  headers.set('content-type', contentType);
+
+  // Remove CORS
+  headers.delete('Access-Control-Allow-Origin');
+  headers.delete('Vary');
+
+  // Set content type
+  const contentType = getContentType(getExtension(path));
+  headers.set('Content-Type', contentType);
 
   return new Response(response.body, {
-    headers,
     status: response.status,
     statusText: response.statusText,
+    headers,
   });
 }
 
@@ -52,7 +46,14 @@ function getContentType(extension: string): string {
       return 'image/jpeg';
     case 'svg':
       return 'image/svg+xml';
+    case 'ico':
+      return 'image/x-icon';
     default:
       return 'text/plain';
   }
+}
+
+function getExtension(path: string): string {
+  const index = path.lastIndexOf('.');
+  return path.slice(index + 1);
 }

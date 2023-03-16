342c2e40918b237a240c55d0fa3d97e305f592d1
Toasticle <92263505+Toasticle@users.noreply.github.com>
2023-03-16 22:15:25 +0000

    Add files via upload

--- deploy.ts
+++ deploy.ts
@@ -1,8 +1,21 @@
-addEventListener("fetch", (event) => {
+addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request: Request): Promise<Response> {
-  const response = await fetch(`https://raw.githubusercontent.com/Toasticle/test123/main/index.html`);
+  // Extract the path from the incoming request URL
+  const url = new URL(request.url);
+  var path = url.pathname;
+
+  if (path == "/") {
+     path = "/index.html"	
+  }
+
+  // Construct the URL for the remote file
+  const remoteUrl = `https://raw.githubusercontent.com/Toasticle/test123/main${path}`;
+
+  // Fetch the remote file and return it as the response
+  const response = await fetch(remoteUrl);
+
   return response;
 }

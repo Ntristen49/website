424dad73e56bb25afe363b337b2680c0d2f530b9
Toasticle <92263505+Toasticle@users.noreply.github.com>
2023-03-16 22:11:14 +0000

    Delete deploy.ts

--- deploy.ts
+++
@@ -1,8 +0,0 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request: Request): Promise<Response> {
-  const response = await fetch(`https://${request.headers.get("host")}/index.html`);
-  return response;
-}

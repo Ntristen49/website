fdb18a74dca596c802e4b2d33b54cff7e11c7a6f
Toasticle <92263505+Toasticle@users.noreply.github.com>
2023-03-17 23:22:12 +0000

    Add files via upload

--- deploy.ts
+++ deploy.ts
@@ -1,59 +1,18 @@
-addEventListener('fetch', (event) => {
+addEventListener("fetch", (event) => {
   event.respondWith(handleRequest(event.request));
 });
 
 async function handleRequest(request: Request): Promise<Response> {
-  const url = new URL(request.url);
-  let path = url.pathname;
+  const htmlUrl = "https://raw.githubusercontent.com/Toasticle/test123/main/index.html";
+  const response = await fetch(htmlUrl);
+  const htmlContent = await response.text();
 
-  if (path === '/') {
-    path = '/index.html';
-  }
+  // Set the content type of the response to "text/html"
+  const headers = new Headers();
+  headers.set("content-type", "text/html");
 
-  const response = await fetch(`https://raw.githubusercontent.com/Toasticle/test123/main${path}`);
+  // Create a new response with the contents of the HTML file
+  const htmlResponse = new Response(htmlContent, { headers });
 
-  const headers = new Headers(response.headers);
-
-  // Remove CORS
-  headers.delete('Access-Control-Allow-Origin');
-  headers.delete('Vary');
-
-  // Set content type
-  const contentType = getContentType(getExtension(path));
-  headers.set('Content-Type', contentType);
-
-  return new Response(response.body, {
-    status: response.status,
-    statusText: response.statusText,
-    headers,
-  });
-}
-
-function getContentType(extension: string): string {
-  switch (extension) {
-    case 'html':
-      return 'text/html';
-    case 'css':
-      return 'text/css';
-    case 'js':
-      return 'application/javascript';
-    case 'json':
-      return 'application/json';
-    case 'png':
-      return 'image/png';
-    case 'jpeg':
-    case 'jpg':
-      return 'image/jpeg';
-    case 'svg':
-      return 'image/svg+xml';
-    case 'ico':
-      return 'image/x-icon';
-    default:
-      return 'text/plain';
-  }
-}
-
-function getExtension(path: string): string {
-  const index = path.lastIndexOf('.');
-  return path.slice(index + 1);
+  return htmlResponse;
 }

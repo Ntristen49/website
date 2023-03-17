48967f261abf42b8088e817aa5fcc903cbb6b03a
Toasticle <92263505+Toasticle@users.noreply.github.com>
2023-03-17 23:27:12 +0000

    Add files via upload

--- deploy.ts
+++ deploy.ts
@@ -3,16 +3,36 @@ addEventListener("fetch", (event) => {
 });
 
 async function handleRequest(request: Request): Promise<Response> {
-  const htmlUrl = "https://raw.githubusercontent.com/Toasticle/test123/main/index.html";
-  const response = await fetch(htmlUrl);
-  const htmlContent = await response.text();
+  const url = new URL(request.url);
 
-  // Set the content type of the response to "text/html"
-  const headers = new Headers();
-  headers.set("content-type", "text/html");
+  // Serve index.html for "/" or "/index.html"
+  if (url.pathname === "/" || url.pathname === "/index.html") {
+    const response = await fetch("https://raw.githubusercontent.com/Toasticle/test123/main/index.html");
+    const headers = new Headers({ "content-type": "text/html" });
+    return new Response(await response.text(), { headers });
+  }
 
-  // Create a new response with the contents of the HTML file
-  const htmlResponse = new Response(htmlContent, { headers });
+  // Serve app.f55877a1.css for "/css/app.f55877a1.css"
+  if (url.pathname === "/css/app.f55877a1.css") {
+    const response = await fetch("https://raw.githubusercontent.com/Toasticle/test123/main/css/app.f55877a1.css");
+    const headers = new Headers({ "content-type": "text/css" });
+    return new Response(await response.text(), { headers });
+  }
 
-  return htmlResponse;
+  // Serve 838.35daa32b.css for "/css/838.35daa32b.css"
+  if (url.pathname === "/css/838.35daa32b.css") {
+    const response = await fetch("https://raw.githubusercontent.com/Toasticle/test123/main/css/838.35daa32b.css");
+    const headers = new Headers({ "content-type": "text/css" });
+    return new Response(await response.text(), { headers });
+  }
+
+  // Serve favicon.ico for "/favicon.ico"
+  if (url.pathname === "/favicon.ico") {
+    const response = await fetch("https://raw.githubusercontent.com/Toasticle/test123/main/favicon.ico");
+    const headers = new Headers({ "content-type": "image/x-icon" });
+    return new Response(await response.arrayBuffer(), { headers });
+  }
+
+  // Return 404 Not Found for all other requests
+  return new Response("Not Found", { status: 404 });
 }
